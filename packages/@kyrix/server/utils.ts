e885833065aa1bfa62b82e8bd980a4e7d4da68da
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-09-14 20:57:45 +0530

    feat: added helper function for serving static assets

--- packages/@kyrix/server/utils.ts
+++ packages/@kyrix/server/utils.ts
@@ -1,3 +1,8 @@
+import type { ServerResponse } from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import mime from 'mime';
+
 import { SSRData } from './config';
 
 export type KyrixSSRHandlerReturnType = {
@@ -15,3 +20,20 @@ export type SSRRoute<T> = {
   path: string;
   handler: KyrixSSRHandler<T>;
 };
+
+export async function serveStatic(res: ServerResponse, absolutePath: string) {
+  try {
+    const filePath = path.resolve(process.cwd(), absolutePath);
+    const file = await fs.readFile(filePath, {
+      encoding: 'utf-8',
+    });
+
+    const fileExt = path.extname(filePath);
+    const mimeType = mime.getType(fileExt.slice(1));
+
+    res.appendHeader('Content-Type', mimeType ?? 'text/plain');
+    res.end(file);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
